0b4a6b54e766bd7e4350cbf1673a10cbd65f10f0
Beninigmal <beninigmal@gmail.com>
2020-08-20 00:22:02 -0300

    .

--- src/components/PaginaFrontEnd/index.js
+++ src/components/PaginaFrontEnd/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../PaginaFrontEnd/style.css';
 import BotaoVoltar from '../BotaoVoltar';
-import { Link } from 'react-router-dom';
+
 
 function PaginaFrontEnd () {
     return (
@@ -11,7 +11,7 @@ function PaginaFrontEnd () {
             <p>React é uma bibliotéca JavaScript para criação de interfaces de usuário, e nesse contexto, uma das tecnologias mais utilizadas pelo mercado. No guia abaixo você aprenderá quais os pré-requisitos para iniciar no React do jeito certo e como criar as suas primeiras SPAs com ela.</p>
 
             </div>
-            <Link to="https://www.devmedia.com.br/react/">Guia de React</Link>
+            <a href="https://www.devmedia.com.br/react/">Guia de React</a>
             <BotaoVoltar />
         </section>
     );
